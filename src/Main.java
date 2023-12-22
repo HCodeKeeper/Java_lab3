da429245ebbd9d5010028872d80435efee134ae1
unknown <ivannikovmax04@gmail.com>
2023-12-22 17:28:01 +0200

    Handled nulls in both text and comparison words. Fixed the search algorithm using pattern matching at boundaries

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,33 +8,44 @@ public class Main {
         int c3 = my_variant % 3;
         int c13 = my_variant % 17;
         System.out.println("My variant: " + my_variant + ". C3(StringBuilder) = " + c3 + ". C13 = " + c13);
+        StringBuilder text;
+        try {
+            text = new StringBuilder("Elevators have become an integral part of our daily lives," +
+                    " especially in cities where skyscrapers and high-rise buildings are common. They provide us" +
+                    " with a convenient and efficient way to move between different floors, whether it is in a" +
+                    " residential building, office complex, or shopping mall. However, many people often have concerns" +
+                    " about the safety of elevators, raising questions about the likelihood of them malfunctioning and" +
+                    " causing accidents. So, how safe are elevators really?");
+        }
+        catch (NullPointerException e){
+            System.out.println("Text cannot be null. Terminating the program");
+            return;
+        }
+        String[] wordsArray = {"They", "are", "is", "plant", "build"};
 
-        StringBuilder text = new StringBuilder("Elevators have become an integral part of our daily lives," +
-                " especially in cities where skyscrapers and high-rise buildings are common. They provide us" +
-                " with a convenient and efficient way to move between different floors, whether it is in a" +
-                " residential building, office complex, or shopping mall. However, many people often have concerns" +
-                " about the safety of elevators, raising questions about the likelihood of them malfunctioning and" +
-                " causing accidents. So, how safe are elevators really?");
-        String[] wordsArray = {"They", "are", "is"};
-
-        Map<String, Integer> wordCountMap = new HashMap<>();
+        Map<String, Integer> wordCountMap = new LinkedHashMap<>();
 
         for (String word : wordsArray) {
+            if (word == null){
+                System.out.println("Null was found among words to compare to. Terminating the program");
+                return;
+            }
             wordCountMap.put(word, 0);
         }
 
         for (String word : wordsArray) {
-            int index = text.indexOf(word);
+            Pattern pattern = Pattern.compile("\\b" + word + "\\b");
+            Matcher matcher = pattern.matcher(text);
 
-            while (index != -1) {
+            while (matcher.find()) {
                 wordCountMap.put(word, wordCountMap.get(word) + 1);
-                if (index + 1 < text.length()){
-                    index = text.indexOf(word, index + 1);
-                }
             }
         }
 
-        for (Map.Entry<String, Integer> entry : wordCountMap.entrySet()) {
+        List<Map.Entry<String, Integer>> sortedEntries = new ArrayList<>(wordCountMap.entrySet());
+        sortedEntries.sort(Map.Entry.<String, Integer>comparingByValue().reversed());
+
+        for (Map.Entry<String, Integer> entry : sortedEntries) {
             System.out.println("Word '" + entry.getKey() + "' appears in " + entry.getValue() + " occurrences.");
         }
     }
